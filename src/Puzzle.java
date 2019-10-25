23bb43c4d6c414ce10216f504d0885cd2d4e8164
Fzekiqi <fazlizekiqi1@hotmail.com>
2019-10-25 21:50:37 +0200

    newGame button

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -15,6 +15,7 @@ public class Puzzle extends JFrame {
     ArrayList<JButton> buttons = new ArrayList<>();
     JPanel panel = new JPanel();
     JButton invisibleButton = new JButton();
+    JButton newGame=new JButton("New Game");
 
     Puzzle() {
         initJFrame();
@@ -26,6 +27,10 @@ public class Puzzle extends JFrame {
         invisibleButton.setText("16");
         invisibleButton.setVisible(false);
         panel.setLayout(new GridLayout(4, 4));
+
+        newGame.setVisible(false);
+        newGame.addActionListener(startNewGame);
+        add(newGame,BorderLayout.NORTH);
         add(panel);
         panel.add(invisibleButton);
         setLocationRelativeTo(null);
@@ -48,7 +53,7 @@ public class Puzzle extends JFrame {
     }
 
     public void shuffleButtons() {
-        Collections.shuffle(buttons);
+    //  Collections.shuffle(buttons);
         for (int i = 0; i < buttons.size(); i++) {
             panel.add(buttons.get(i));
         }
@@ -62,7 +67,6 @@ public class Puzzle extends JFrame {
                 JButton button = buttons.get(i);
 
                 if (
-
                         ((
                                 (invisibleButton.getY() == button.getY()) &&
                                         button.getX() == invisibleButton.getX() - invisibleButton.getWidth() |
@@ -77,13 +81,11 @@ public class Puzzle extends JFrame {
 
                     moveTiles(button);
                     isGameOver();
-
                 }
                 break;
             }
         }
     };
-
 
     public void moveTiles(JButton button) {
         Point temp = new Point(button.getLocation());
@@ -102,10 +104,26 @@ public class Puzzle extends JFrame {
                 Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16")
                         .allMatch((buttonNumber) -> buttonNumber.equals(buttons.get(Integer.parseInt(buttonNumber) - 1).getText()))
         ) {
+
             JOptionPane.showMessageDialog(null, "Du har vunnit Fazli!");
+            newGame.setVisible(true);
         }
     }
 
+
+    ActionListener startNewGame= e -> {
+        for (JButton button : buttons ){
+            buttons.remove(button);
+            panel.remove(button);
+        }
+
+        getContentPane().remove(panel);
+        getContentPane().remove(newGame);
+        getContentPane().repaint();
+
+
+        initJFrame();
+    };
 
     public static void main(String[] args) {
         new Puzzle();
---
+++ src/Testing.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Testing {
+
+    public static void main(String[] args) {
+        List<String> string=new ArrayList<>();
+
+        string.add("Fazli ");
+        string.add("Zekiqi");
+
+        string.clear();
+
+
+    }
+
+}
